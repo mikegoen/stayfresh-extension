91fd5388278252c10358c6b3a56c980a0a8835d8
mikegoen <mikegoen@users.noreply.github.com>
2015-10-29 17:31:35 -0700

    add refresh modifier

--- popup.js
+++ popup.js
@@ -1,22 +1,24 @@
 var targetUrls;
+var refresh_interval = document.getElementById('interval'); 
+var storage = chrome.storage.sync;
 
-//add to html list
+// get info on popup load
+storage.get(null, function(items) {
+	//update page list
+	for (var i = 0; i < items.urls.length; i++){
+			addItem(items.urls[i])
+		}	
+	targetUrls = items.urls;
+	refresh_interval.value = items.interval;
+});
+
+//add to page list
 function addItem(text){
 	var m = document.createElement('li');
 	m.textContent = text;
 	mylist = document.getElementById('url-list');
 	mylist.appendChild(m);
 };
-
-// get urls on popup load
-chrome.storage.sync.get(null, function(items) {
-	//update html list
-	for (var i = 0; i < items.urls.length; i++){
-			addItem(items.urls[i])
-		}	
-
-	targetUrls = items.urls
-});
 
 function getActiveTab(callback) {
 	chrome.tabs.query({'currentWindow': true, 'active': true}, function(tab){
@@ -31,16 +33,16 @@ function addUrl(){
 		targetUrls.push(tab[0]['url']); //add to global variable
 
 		//delete old url array, then add updated one
-		chrome.storage.sync.remove('urls', function() {
-			chrome.storage.sync.set({'urls': targetUrls});
+		storage.remove('urls', function() {
+			storage.set({'urls': targetUrls});
 		});
 	});
 };
 
 //button function that clears url list and storage
 function clearUrls() {
-	chrome.storage.sync.remove('urls', function() {
-			chrome.storage.sync.set({'urls': []});
+	storage.remove('urls', function() {
+			storage.set({'urls': []});
 		});
 
 	var pagelist = document.getElementById('url-list');
@@ -53,6 +55,12 @@ function clearUrls() {
 document.addEventListener('DOMContentLoaded', function() {
 	var addButton = document.getElementById('add-url');
 	var clearButton = document.getElementById('clear-all');
+
 	addButton.addEventListener('click', addUrl);
+
 	clearButton.addEventListener('click', clearUrls);
+
+	refresh_interval.addEventListener('input', function() {
+		storage.set({'interval': Number(refresh_interval.value)});
+	});
 });
