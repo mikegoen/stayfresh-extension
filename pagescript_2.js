882d6cfa024ff27a65b1f46e3d31a5d0c60d1206
mikegoen <mikejg4@gmail.com>
2016-02-13 20:53:08 -0800

    delete

--- pagescript_2.js
+++
@@ -1,9 +0,0 @@
-function refresh() {
-	location.reload(true);
-};
-
-//refresh page after time period
-var stayfresh_timer = setTimeout(refresh, 10000);
-
-//cancel refresh if mouse is moved
-document.onmousemove = function(){clearTimeout(stayfresh_timer);}
